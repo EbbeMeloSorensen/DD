c8530095bfe959a280ddf0c1b0ef06d923f86640
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2025-10-12 09:31:01 +0200

    -

---
+++ DD.ViewModel/TeamStatsListItemViewModel.cs
@@ -0,0 +1,21 @@
+﻿using GalaSoft.MvvmLight;
+using DD.Domain;
+
+namespace DD.ViewModel;
+
+public class TeamStatsListItemViewModel : ViewModelBase
+{
+    private int _count;
+
+    public CreatureType CreatureType { get; set; }
+
+    public int Count
+    {
+        get => _count;
+        set
+        {
+            _count = value;
+            RaisePropertyChanged();
+        }
+    }
+}
